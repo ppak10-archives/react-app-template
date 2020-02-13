8a0a57e875e47b136c0ab31fbbb7959439018be5
Peter <ppak10@gmail.com>
2020-02-13 14:22:13 -0800

    Updates react-app-elements to 0.0.13

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,22 +8,31 @@ import React from 'react';
 import {Card, Grid, Layout, Sidebar} from 'react-app-elements';
 // import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+// Constants
+const IMAGES = 100;
+const MAX_SIZE = 1000;
+
+// Utils
+import {getRandomInt} from '../utils';
 export default function App() {
+  
+  // JSX
+  const polaroidJSX = [...Array(IMAGES)].map((e, index) => (
+    <Card.Polaroid
+      key={index}
+      scale={1}
+      src={`https://picsum.photos/${getRandomInt(MAX_SIZE)}/${getRandomInt(MAX_SIZE)}`}
+    />
+  ));
+
   return (
-<div className="layout-row">
-      <Layout.PageLayout>
+    <div className="layout-row">
+      <Layout.Page>
         <div>Hello World</div>
         <Grid.PolaroidContainer>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
-          <Card.Polaroid/>
+          {polaroidJSX}
         </Grid.PolaroidContainer>
-      </Layout.PageLayout>
+      </Layout.Page>
       <Sidebar.DrawerSidebar side="right">
         <div>Hello</div>
         <div>World</div>
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+/**
+ * utils.js
+ * Common functions used within the app
+ */
+
+export const getRandomInt = (max) =>
+  Math.floor(Math.random() * Math.floor(max));
